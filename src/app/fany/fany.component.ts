aed4f54690441fb4537ea55fc247fcdee7b4d66f
iamphanye <nyamilkgore@gmail.com>
2025-03-27 10:17:04 -0600

    Componente de fany

---
+++ src/app/fany/fany.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fany',
+  imports: [],
+  templateUrl: './fany.component.html',
+  styleUrl: './fany.component.scss'
+})
+export class FanyComponent {
+
+}
